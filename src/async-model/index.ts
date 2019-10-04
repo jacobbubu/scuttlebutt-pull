43c4fc4b8921f5998c29fb62697c5e0408d0687f
jacobbubu <rong.shen@gmail.com>
2019-10-04 16:29:16 +0800

    feat: add 'changedByPeer' to async-model

--- src/async-model/index.ts
+++ src/async-model/index.ts
@@ -76,9 +76,16 @@ class AsyncModel extends Scuttlebutt {
     await this.store.set(key, update)
 
     this.emit.apply(this, ['update', update])
-    this.emit('change', key, update[UpdateItems.Data][ModelValueItems.Value])
-    this.emit('change:' + key, update[UpdateItems.Data][ModelValueItems.Value])
-
+    this.emit('changed', key, update[UpdateItems.Data][ModelValueItems.Value])
+    this.emit('changed:' + key, update[UpdateItems.Data][ModelValueItems.Value])
+    if (update[UpdateItems.SourceId] !== this.id) {
+      this.emit(
+        'changedByPeer',
+        key,
+        update[UpdateItems.Data][ModelValueItems.Value],
+        update[UpdateItems.From]
+      )
+    }
     return true
   }
 
