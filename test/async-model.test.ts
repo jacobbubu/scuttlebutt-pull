87d087d1c3882033f63f9609a08f79c0b24cf2a2
jacobbubu <rong.shen@gmail.com>
2019-10-04 16:30:59 +0800

    test: add test case to async-model

---
+++ test/async-model.test.ts
@@ -0,0 +1,57 @@
+import { AsyncModel, link } from '../src'
+
+describe('async-model', () => {
+  const expected = {
+    key: 'foo',
+    valueA: 'changed by A',
+    valueB: 'changed by B'
+  }
+
+  it('local change', done => {
+    const a = new AsyncModel('A')
+
+    let c = 2
+    a.on('changed', (key, value) => {
+      expect(key).toBe(expected.key)
+      expect(value).toBe(expected.valueA)
+      if (!--c) done()
+    })
+
+    a.on(`changed:${expected.key}`, value => {
+      expect(value).toBe(expected.valueA)
+      if (!--c) done()
+    })
+
+    a.set(expected.key, expected.valueA)
+  })
+
+  it('change in two-ways', done => {
+    const a = new AsyncModel('A')
+    const b = new AsyncModel('B')
+
+    const s1 = a.createStream({ name: 'a->b' })
+    const s2 = b.createStream({ name: 'b->a' })
+
+    a.set(expected.key, expected.valueA)
+
+    b.on('changedByPeer', async (key, value, sourceId) => {
+      expect(sourceId).toBe(a.id)
+      expect(key).toBe(expected.key)
+      expect(value).toBe(expected.valueA)
+
+      expect(await b.get(expected.key)).toBe(expected.valueA)
+
+      a.on('changedByPeer', async (key, value, sourceId) => {
+        expect(sourceId).toBe(b.id)
+        expect(key).toBe(expected.key)
+        expect(value).toBe(expected.valueB)
+        expect(await a.get(expected.key)).toBe(expected.valueB)
+        done()
+      })
+
+      b.set(expected.key, expected.valueB)
+    })
+
+    link(s1, s2)
+  })
+})
